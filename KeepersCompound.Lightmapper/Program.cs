07e7fb1801e2fab0f24085ff6d8acd373c5db609
Jarrod Doyle <thejarroddoyle@gmail.com>
2024-12-09 16:03:27 +0000

    Change CLI library

--- KeepersCompound.Lightmapper/Program.cs
+++ KeepersCompound.Lightmapper/Program.cs
@@ -1,36 +1,36 @@
-using CliFx;
-using CliFx.Attributes;
-using IConsole = CliFx.Infrastructure.IConsole;
+using DotMake.CommandLine;
 
 namespace KeepersCompound.Lightmapper;
 
 internal static class Program
 {
-    public static async Task<int> Main() =>
-        await new CliApplicationBuilder().AddCommandsFromThisAssembly().Build().RunAsync();
+    public static async Task<int> Main(string[] args) =>
+        await Cli.RunAsync<LightCommand>(args);
 }
 
-[Command(Description = "Compute lightmaps for a NewDark .MIS/.COW")]
-public class LightCommand : ICommand
+[CliCommand(Description = "Compute lightmaps for a NewDark .MIS/.COW")]
+public class LightCommand
 {
-    [CommandParameter(0, Description = "The path to the root Thief installation.")]
-    public required string InstallPath { get; init; }
-    [CommandParameter(1, Description = "The folder name of the fan mission. For OMs this is blank.")]
-    public required string CampaignName { get; init; }
-    [CommandParameter(2, Description = "The name of the mission file including extension.")]
-    public required string MissionName { get; init; }
-    [CommandOption("output", 'o', Description = "Name of output file excluding extension.")]
-    public string OutputName { get; init; } = "kc_lit";
+    [CliArgument(Description = "The path to the root Thief installation.")]
+    public required string InstallPath { get; set; }
+    
+    [CliArgument(Description = "The folder name of the fan mission. For OMs this is blank.")]
+    public required string CampaignName { get; set; }
+    
+    [CliArgument(Description = "The name of the mission file including extension.")]
+    public required string MissionName { get; set; }
+    
+    [CliOption(Description = "Name of output file excluding extension.")]
+    public string OutputName { get; set; } = "kc_lit";
 
-    public ValueTask ExecuteAsync(IConsole console)
+    public void Run()
     {
         Timing.Reset();
-            
+             
         var lightMapper = new LightMapper(InstallPath, CampaignName, MissionName);
         lightMapper.Light();
         lightMapper.Save(OutputName);
-            
+         
         Timing.LogAll();
-        return default;
     }
 }
