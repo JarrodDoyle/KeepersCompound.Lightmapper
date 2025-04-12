2cc28ee0cdfef038527f64b93de6e5c8e7225e1a
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-04-12 11:42:00 +0100

    Only enable run button when required fields are set and the install directory exists

--- KCLight.Gui/ViewModels/MainWindowViewModel.cs
+++ KCLight.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,24 +1,35 @@
-﻿using Avalonia;
+﻿using System.IO;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.ComponentModel;
 using KeepersCompound.Lighting;
 
 namespace KCLight.Gui.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string InstallPath { get; set; } = "";
-    public string CampaignName { get; set; } = "";
-    public string MissionName { get; set; } = "";
-    public string OutputName { get; set; } = "kc_lit";
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRun))]
+    private string _installPath = "";
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRun))]
+    private string _campaignName = "";
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRun))]
+    private string _missionName = "";
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRun))]
+    private string _outputName = "kc_lit";
+
     public bool FastPvs { get; set; }
-    
+    public bool CanRun => Directory.Exists(InstallPath) && CampaignName != "" && MissionName != "" && OutputName != "";
+
     public bool Run()
     {
         if (InstallPath == "" || CampaignName == "" || MissionName == "" || OutputName == "")
         {
             return false;
         }
-        
+
         var lightMapper = new LightMapper(InstallPath, CampaignName, MissionName);
         lightMapper.Light(FastPvs);
         lightMapper.Save(OutputName);
