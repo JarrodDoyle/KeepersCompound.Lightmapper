2e92f3d1bef5f4d3067fe5959d5de752b314e008
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-04-12 11:04:44 +0100

    Add ViewModel fields for lightmapper settings and bind them

--- KCLight.Gui/ViewModels/MainWindowViewModel.cs
+++ KCLight.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace KCLight.Gui.ViewModels;
+﻿using Avalonia;
+
+namespace KCLight.Gui.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string InstallPath { get; set; } = "";
+    public string CampaignName { get; set; } = "";
+    public string MissionName { get; set; } = "";
+    public string OutputName { get; set; } = "kc_lit";
+    public bool FastPvs { get; set; }
 }
