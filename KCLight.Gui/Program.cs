d361dffa986a9c02344e3f98b3ed00a82ab3a58c
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-04-13 18:20:05 +0100

    Configure logging in GUI

--- KCLight.Gui/Program.cs
+++ KCLight.Gui/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using System;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace KCLight.Gui;
 
@@ -9,12 +11,30 @@ internal static class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        ConfigureLogger();
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .LogToTrace();
+
+    private static void ConfigureLogger()
+    {
+        const string outputTemplate = "{Timestamp:HH:mm:ss.fff} [{Level}] {Message:lj}{NewLine}{Exception}";
+        var logPath = $"{AppDomain.CurrentDomain.BaseDirectory}/logs/{DateTime.Now:yyyyMMdd_HHmmss}.log";
+        var config = new LoggerConfiguration();
+#if DEBUG
+        config.MinimumLevel.Debug();
+#endif
+
+        Log.Logger = config
+            .WriteTo.Console(theme: AnsiConsoleTheme.Sixteen, outputTemplate: outputTemplate)
+            .WriteTo.File(logPath, outputTemplate: outputTemplate)
+            .CreateLogger();
+    }
 }
