36211c80dd9fa9dbda8feb14c51664778209c75a
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-04-18 16:12:17 +0100

    Show drop down filter box on focus of `CampaignName` and `MissionName`

--- KCLight.Gui/Views/MainWindow.axaml.cs
+++ KCLight.Gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Serilog;
 
 namespace KCLight.Gui.Views;
 
@@ -7,5 +9,17 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        MissionNameBox.GotFocus += OnAutocompleteFocus;
+        CampaignNameBox.GotFocus += OnAutocompleteFocus;
+    }
+
+    private static void OnAutocompleteFocus(object? sender, GotFocusEventArgs e)
+    {
+        if (sender is AutoCompleteBox box)
+        {
+            box.IsDropDownOpen = true;
+            e.Handled = true;
+        }
     }
 }
