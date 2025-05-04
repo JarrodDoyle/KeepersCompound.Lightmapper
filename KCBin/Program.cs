a56bba5624a67a03c9ae1faec2a2504f65a7a8f1
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-05-04 10:55:32 +0100

    Add dummy command line interface

--- KCBin/Program.cs
+++ KCBin/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DotMake.CommandLine;
+
+namespace KCBin;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Cli.Run<RootCommand>(args);
+    }
+}
+
+[CliCommand(Description = "Work with NewDark .BIN files.")]
+public class RootCommand
+{
+    [CliCommand(Description = "Model file handling")]
+    public class ModelCommand
+    {
+        [CliCommand(Description = "Dump information about a model")]
+        public class DumpCommand
+        {
+        }
+
+        [CliCommand(Description = "Import a model")]
+        public class ImportCommand
+        {
+        }
+
+        [CliCommand(Description = "Export a model")]
+        public class ExportCommand
+        {
+        }
+    }
+
+    [CliCommand(Description = "Mesh file handling")]
+    public class MeshCommand
+    {
+        [CliCommand(Description = "Dump information about a mesh")]
+        public class DumpCommand
+        {
+        }
+
+        [CliCommand(Description = "Import a mesh")]
+        public class ImportCommand
+        {
+        }
+
+        [CliCommand(Description = "Export a mesh")]
+        public class ExportCommand
+        {
+        }
+    }
+}
