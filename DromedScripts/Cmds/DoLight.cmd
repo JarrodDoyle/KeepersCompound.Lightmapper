7a4fd9f64473592e74b49d6f959c8413bc99952e
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-06-03 18:44:46 +0100

    Fix saving in FMs with `.` in their name

--- DromedScripts/Cmds/DoLight.cmd
+++ DromedScripts/Cmds/DoLight.cmd
@@ -6,8 +6,8 @@ brush_set_time 0
 force_wr_up2date
 
 eval world set old_world %s
-save_cow KCLight
 eval world mprint %s
+save_cow KCLight.cow
 run_sys_wait "Tools\KCTools\run_sys_wait_light.cmd"
 load_file KCLight.cow
 eval old_world set world %s
